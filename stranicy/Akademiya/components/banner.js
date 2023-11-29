ee0b42ec64baf9a75825d6664e362ae9170eec54
mawglih <mawglih@gmail.com>
2023-11-29 16:36:00 -0500

    vika academy start

---
+++ stranicy/Akademiya/components/banner.js
@@ -0,0 +1,20 @@
+let animatedContainer = document.querySelector('.animated-banner-container');
+let j = 0;
+
+while(j < 200) {
+    let star = document.createElement('i');
+    let x = Math.floor(Math.random() * window.innerWidth);
+    star.style.left = `${x}px`;
+    let y = Math.floor(Math.random() * window.innerHeight * .5);
+    star.style.top = `${y}px`;
+
+    let size = Math.random() * 4;
+    star.style.width = `${1 + size}px`;
+    star.style.height = `${1 + size}px`;
+
+    let duration = Math.random() * 2;
+    star.style.animationDuration = `${duration + 1}s`
+
+    animatedContainer.appendChild(star);
+    j++;
+}
